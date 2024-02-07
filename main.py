57aafd55c6dd16d5561cb095ad04f0d7673dd2a0
lmraine <lraine@fhu.edu>
2024-02-07 10:42:55 -0600

    soln

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-#REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+    product = 0
+    total = 0
+
+    for i in range(len(list1)):
+        product = list1[i] * list2[i]
+        total += product
+    
+    return total
 
 if __name__ == '__main__':
-    #REMOVE PASS AND your code goes here
-    pass
+   # Input list A
+    list_A = [int(n) for n in input().split()]
+
+    # Input list B
+    list_B = [int(n) for n in input().split()]
+
+    sum_product = sum_of_products(list_A, list_B)
